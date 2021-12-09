17dd07d0b9bf3fb695add83b14e8765482b73f87
ItzArima <skaypaprilia@gmail.com>
2021-12-09 16:11:16 +0100

    styled main: milestone 1 end

--- components/main.php
+++ components/main.php
@@ -6,12 +6,14 @@
     <div class="disks-container">
         <?php foreach($discs as $key => $value) { ?>
         <div class="disk">
-            <img src="<?php echo $value['poster'];?>" alt="">
-            <div class="disk-desc">    
-                <h1><?php echo $value['title'];?></h1>
-                <p><?php echo $value['author']?></p>
-                <p><?php echo $value['year']?></p>
-            </div>
+            <div class="disk-card">
+                <img src="<?php echo $value['poster'];?>" alt="">
+                <div class="disk-desc">    
+                    <h2><?php echo $value['title'];?></h2>
+                    <p><?php echo $value['author']?></p>
+                    <p><?php echo $value['year']?></p>
+                </div>
+            </div>    
         </div>
         <?php } ?>
     </div>
